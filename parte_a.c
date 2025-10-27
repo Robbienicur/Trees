a3de058fc3ae10dc32355ce922c54957452e5b1e
Robbienicur <robbienicur@gmail.com>
2025-10-27 15:37:56 +0000

    Parte A

---
+++ parte_a.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define MAX 10
+int root = 0;              // index of the root
+int numElements = 10;      // total stored elements
+char nodes[MAX] = {
+  /*0*/ 'G',
+  /*1*/ 'D', /*2*/ 'I',
+  /*3*/ 'B', /*4*/ 'F', /*5*/ 'H', /*6*/ 'J',
+  /*7*/ 'A', /*8*/ 'C', /*9*/ 'E'
+};
+
+int main(void) {
+  return 0;
+}
